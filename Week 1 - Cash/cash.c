f54d9bf73861657c08cbf038893cb7058fab06cb
JCM2020 <67202242+JCM2020@users.noreply.github.com>
2020-07-02 12:30:59 +0100

    Update cash.c

--- Week 1 - Cash/cash.c
+++ Week 1 - Cash/cash.c
@@ -33,19 +33,19 @@ int main(void)
         coins++;
     }
 
-    while (cents >= 10)
+    while (cents >= 10 && cents < 25)
     {
         cents = cents - 10;
         coins++;
     }
 
-    while (cents >= 5)
+    while (cents >= 5 && cents < 10)
     {
         cents = cents - 5;
         coins++;
     }
 
-    while (cents >= 1)
+    while (cents >= 1 && cents < 5)
     {
         cents = cents - 1;
         coins++;
